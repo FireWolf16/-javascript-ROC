ab9ac1e4620ee30f4920eadc640815af49cf3639
patrick <patrick.laranjo@gmail.com>
2019-04-01 14:14:18 +0200

    tweede les

---
+++ javascript-dom/VulMetJS.js
@@ -0,0 +1,14 @@
+document.getElementById("paginatitel").innerText = "mijn geweldige pagina";
+document.getElementById("subkop").innerText = "mijn geweldige subkop";
+document.getElementById("aap1").src = "https://nos.nl/data/image/2017/09/12/416754/1008x567.jpg";
+document.getElementById("aap2").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Pan_troglodytes_%28male%29.jpg/280px-Pan_troglodytes_%28male%29.jpg";
+
+
+
+
+
+
+
+
+
+
---
+++ javascript-dom/mijntweedescript.js
@@ -0,0 +1,10 @@
+let mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    AantalWielen: "4",
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
+document.getElementById("grotekop").innerHTML = mijnauto.kleur;
+//not woring//
