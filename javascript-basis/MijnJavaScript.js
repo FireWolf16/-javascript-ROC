9bfd4a5863977eb43b433bed904384bfebbcdaab
patrick <patrick.laranjo@gmail.com>
2019-04-01 13:24:57 +0200

    first test

---
+++ javascript-basis/MijnJavaScript.js
@@ -0,0 +1,17 @@
+let a = 10;
+if(a > 9) {
+    console.log("hallo a is groten dan 9 hoor")
+}
+
+for (var i = 0; i<= 10; i++){
+console.log("i heeft een waarde " +i)
+}
+
+let mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    AantalWielen: "4",
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
